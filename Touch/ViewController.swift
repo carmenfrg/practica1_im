d5c819b89197557a4a8bde9e31789e24bf4022e7
Rafael Ruiz Marquez <rafaelruiz@MacBook-Pro-de-Rafael.local>
2017-09-05 10:46:25 -0600

    Initial Commit

---
+++ Touch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touch
+//
+//  Created by Rafael Ruiz Marquez on 05/09/17.
+//  Copyright © 2017 Carmen Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
